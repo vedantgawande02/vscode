78324cf2a0f036161af41db0869ba5468a552142
vedantgawande02 <103427624+vedantgawande02@users.noreply.github.com>
2022-04-22 15:21:00 +0530

    Add files via upload

---
+++ simple interest.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+   int p,n;
+   float r,SI;
+   p=1000;
+   r=2.5;
+   n=3;
+   SI=p*r*n/100;
+   printf("Enter the value of SI");
+   scanf("%f",&d);
+    return0;
+}
